6f2904c649aa7a39bab65deaa86309a4425bb7fc
Ozodbek <ozodbekkarimov100@gmail.com>
2023-12-14 23:40:36 +0100

    Initial commit

---
+++ app/src/main/java/pl/ozodbek/mviarchitecture/MainActivity.kt
@@ -0,0 +1,11 @@
+package pl.ozodbek.mviarchitecture
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
